90a6bd64c4644392a59910005dca3c379f3f587c
Kai L <kailin8591@gmail.com>
2022-06-07 23:23:15 -0400

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const submitForm = document.querySelector("form")
+  submitForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const newText = document.querySelector("#new-task-description")
+    const newList = document.createElement("li")
+    if (newText.value === "") {
+      return; //ends the function and not do anything after 
+    }
+    newList.textContent = newText.value;
+    const newItem = document.querySelector("#tasks")
+    newItem.appendChild(newList)
+    newText.value = "" //set to empty string so that it clear the input box and show the placeholder again
+  });
 });
